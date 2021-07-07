516a22f620e086ff6b093ac09042932b0eaf5410
uros <urosrajkovic011@hotmail.com>
2021-07-07 11:36:49 +0200

    new constant

--- classes/Constants.php
+++ classes/Constants.php
@@ -3,6 +3,7 @@
     class Constants{
 
       public static $firstNameCharacters = "Your first name must be between 25 and 2 characters";
+      public static $lastNameCharacters = "Your last name must be between 25 and 2 characters";
       public static $emailCharacters = "Your email must be between 25 and 2 characters";
       public static $passwordCharacters = "Your password must be between 25 and 2 characters";
     }
